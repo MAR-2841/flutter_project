7b23bd9253f632cda1504aa12dcf1e6072cbbabe
MAR-2841 <muhammadaliramzan98@gmail.com>
2026-07-23 17:54:43 +0500

    this is the flutter project

--- lib/help_support_screen.dart
+++ lib/help_support_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HelpSupportScreen extends StatelessWidget {
   const HelpSupportScreen({super.key});
@@ -26,31 +27,35 @@ class HelpSupportScreen extends StatelessWidget {
             Icons.question_answer_outlined,
             "FAQs",
             "Find answers to frequently asked questions.",
+            () => Get.toNamed('/faq'),
           ),
           _buildSupportItem(
             context,
             Icons.contact_support_outlined,
             "Contact Us",
             "Get in touch with our support team.",
+            () => Get.toNamed('/contact_us'),
           ),
           _buildSupportItem(
             context,
             Icons.bug_report_outlined,
             "Report a Bug",
             "Help us improve by reporting issues.",
+            () => Get.toNamed('/report_bug'),
           ),
           _buildSupportItem(
             context,
             Icons.feedback_outlined,
             "Send Feedback",
             "Share your thoughts on how we can improve.",
+            () => Get.toNamed('/feedback'),
           ),
         ],
       ),
     );
   }
 
-  Widget _buildSupportItem(BuildContext context, IconData icon, String title, String subtitle) {
+  Widget _buildSupportItem(BuildContext context, IconData icon, String title, String subtitle, VoidCallback onTap) {
     return Card(
       margin: const EdgeInsets.only(bottom: 16),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
@@ -59,9 +64,7 @@ class HelpSupportScreen extends StatelessWidget {
         title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
         subtitle: Text(subtitle),
         trailing: const Icon(Icons.arrow_forward_ios, size: 16),
-        onTap: () {
-          // Implement specific support action
-        },
+        onTap: onTap,
       ),
     );
   }
